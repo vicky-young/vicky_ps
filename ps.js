4489ee9b77978f9f828742e785db117fa1f022a5
vicky-young <18767166690@163.com>
2021-06-22 16:07:36 +0800

    修改dev代码

--- ps.js
+++ ps.js
@@ -1,6 +1,8 @@
-window.onload =  function() {
-    let box = document.getElementsByClassName('box')[0]
-    box.addEventListener('click', () => {
-        alert('this is test-code')
-    })
-}
+// window.onload =  function() {
+//     let box = document.getElementsByClassName('box')[0]
+//     box.addEventListener('click', () => {
+//         alert('this is test-code')
+//     })
+// }
+
+console.log('这是dev分支的代码')
